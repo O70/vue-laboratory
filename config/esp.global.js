1d1b238178e8b1cb6c685948140e4240b5df2aeb
鬼王 <THRAEX@aliyun.com>
2019-07-12 20:33:47 +0800

    chore: add global.js

---
+++ config/esp.global.js
@@ -0,0 +1,17 @@
+'use strict'
+
+console.info('process.env: ', process.env)
+console.info('NODE_ENV: ', process.env.NODE_ENV)
+console.info('ESP_GATEWAY_HOST:', process.env.ESP_GATEWAY_HOST)
+
+const gh = process.env.ESP_GATEWAY_HOST || 'http://gateway.host.com'
+console.info('gh', gh)
+
+module.exports = {
+  GATEWAY_HOST: gh,
+  FILE_PREFIX: `${gh}/api/fs`,
+  FILE_PATH_ABS(relative_path = '') {
+    const has = relative_path.includes('http://') || relative_path.includes('https://')
+    return has ? relative_path : `${this.FILE_PREFIX}${relative_path}`
+  }
+}
