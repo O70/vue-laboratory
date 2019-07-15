2a04c0874701765be9a29c385f7946e136488716
鬼王 <THRAEX@aliyun.com>
2019-07-15 19:50:35 +0800

    chore: update global variable

--- config/esp.global.js
+++ config/esp.global.js
@@ -1,11 +1,12 @@
 'use strict'
 
-const gh = process.env.ESP_GATEWAY_HOST || 'http://gateway.host.com'
+const GATEWAY_HOST = process.env.ESP_GATEWAY_HOST || 'http://gateway.host.com'
 
 console.info('global')
 module.exports = {
-  GATEWAY_HOST: gh,
-  API_FS: `${gh}/api/fs`,
+  SYS_NAME: process.env.ESP_SYS_NAME,
+  GATEWAY_HOST,
+  API_FS: `${GATEWAY_HOST}/api/fs`,
   FILE_PATH_ABS(relative_path = '') {
     const has = relative_path.includes('http://') || relative_path.includes('https://')
     return has ? relative_path : `${this.API_FS}${relative_path}`
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,23 +1,9 @@
 'use strict'
 
-// const merge = require('webpack-merge')
-//
-// const envs = {}
-// for (let key of Object.keys(process.env)) {
-//   if (typeof process.env[key] !== 'string') {
-//     console.info(key, process.env[key])
-//   }
-//   envs[key] = `"${process.env[key]}"`
-// }
-//
-// module.exports = merge(envs, {
-//   NODE_ENV: '"production"'
-//   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"'
-// })
-
 console.info('prod')
 module.exports = {
   NODE_ENV: '"production"',
-  ESP_GATEWAY_HOST: `"${require('./esp.global').GATEWAY_HOST}"`
+  ESP_SYS_NAME: `"${process.env['npm_package_name']}"`,
+  ESP_GATEWAY_HOST: `"${process.env.ESP_GATEWAY_HOST || 'http://gateway.host.com'}"`
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"'
 }
