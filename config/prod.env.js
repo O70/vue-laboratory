23225323d220ae0ac1289a212169e0c1cbf2d071
鬼王 <THRAEX@aliyun.com>
2019-07-17 06:58:25 +0800

    opt: update global

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,9 +1,8 @@
 'use strict'
 
-console.info('prod')
+console.info('prod', require('./esp.global').GATEWAY_HOST)
 module.exports = {
   NODE_ENV: '"production"',
   ESP_SYS_NAME: `"${process.env['npm_package_name']}"`,
-  ESP_GATEWAY_HOST: `"${process.env.ESP_GATEWAY_HOST || 'http://gateway.host.com'}"`
-  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"'
+  ESP_GATEWAY_HOST: `"${require('./esp.global').GATEWAY_HOST}"`
 }
