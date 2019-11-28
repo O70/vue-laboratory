5959b184c7a467c434e76dca43d68bd1d5fd1bf8
鬼王 <guixiong97@sina.cn>
2019-11-28 10:53:35 +0800

    fix: remove node_modules/webpack-dev-server/client for babel-loader

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -55,7 +55,7 @@ module.exports = {
         include: [
           resolve('src'),
           resolve('test'),
-          resolve('node_modules/webpack-dev-server/client')
+          // resolve('node_modules/webpack-dev-server/client')
         ]
       },
       {
